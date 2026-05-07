1ba71d2b04bc76ada46cd8a5536bd72414bfd469
Jamie Briggs <jamie.briggs@justice.gov.uk>
2026-05-07 13:59:18 +0100

    Added baseline code for Claims API access

---
+++ laa-data-claims-notify-service/src/main/java/uk/gov/justice/laa/dstew/payments/notify/config/rest/WebClientConfiguration.java
@@ -0,0 +1,39 @@
+package uk.gov.justice.laa.dstew.payments.notify.config.rest;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.codec.ClientCodecConfigurer;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.support.WebClientHttpServiceGroupConfigurer;
+import org.springframework.web.service.registry.HttpServiceGroup;
+import org.springframework.web.service.registry.ImportHttpServices;
+import uk.gov.justice.laa.dstew.payments.notify.client.DataClaimsRestClient;
+
+
+@Configuration
+@EnableConfigurationProperties({ClaimsApiProperties.class})
+@ImportHttpServices(
+    types = {
+        DataClaimsRestClient.class,
+    },
+    clientType = HttpServiceGroup.ClientType.WEB_CLIENT)
+public class WebClientConfiguration {
+
+
+  @Bean
+  public WebClientHttpServiceGroupConfigurer groupConfigurer(final ClaimsApiProperties properties) {
+    return groups ->
+        groups.forEachClient(
+            (spec, webClientBuilder) -> {
+              webClientBuilder.exchangeStrategies(
+                  ExchangeStrategies.builder()
+                      .codecs(ClientCodecConfigurer::defaultCodecs)
+                      .build());
+              webClientBuilder.baseUrl(properties.getUrl());
+              webClientBuilder.defaultHeader(
+                  HttpHeaders.AUTHORIZATION, properties.getAccessToken());
+            });
+  }
+}
