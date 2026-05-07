825b26e2f68814b0662a5b3aa2cf0abfcdc5f125
Jamie Briggs <jamie.briggs@justice.gov.uk>
2026-05-07 15:17:53 +0100

    Added baseline code for GOV Notify

---
+++ laa-data-claims-notify-service/src/main/java/uk/gov/justice/laa/dstew/payments/notify/config/notify/GovNotifyProperties.java
@@ -0,0 +1,35 @@
+package uk.gov.justice.laa.dstew.payments.notify.config.notify;
+
+import java.util.EnumMap;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Configuration properties holder for interfacing with the GOV.UK Notify service.
+ *
+ * <p>This class is used to configure and provide access to properties required for
+ * communication with the GOV.UK Notify service, including API keys and template mappings.
+ * It is designed to be loaded automatically from application properties using the
+ * prefix "app.gov-notify".</p>
+ *
+ * <p>Loading template IDs this way via configuration helps keep them secret.</p>
+ *
+ * <p>Properties:</p>
+ * <ul>
+ *   <li>key: The API key used to authenticate with the GOV.UK Notify service.</li>
+ *   <li>templates: A mapping of {@link GovNotifyTemplate} enumerations to their corresponding
+ *  *   template identifiers in the GOV.UK Notify system.</li>
+ * </ul>
+ *
+ * @author Jamie Briggs
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "app.gov-notify")
+public class GovNotifyProperties {
+
+  private String key;
+  private EnumMap<GovNotifyTemplate, String> templates = new EnumMap<>(GovNotifyTemplate.class);
+
+}
---
+++ laa-data-claims-notify-service/src/main/java/uk/gov/justice/laa/dstew/payments/notify/config/notify/GovNotifyTemplate.java
@@ -0,0 +1,7 @@
+package uk.gov.justice.laa.dstew.payments.notify.config.notify;
+
+public enum GovNotifyTemplate {
+  // UPDATE THIS IN FUTURE ONCE EMAILS HAVE BEEN ADDED TO GOV NOTIFY - JAMIE BRIGGS
+  EXAMPLE_EMAIL,
+  EXAMPLE_EMAIL_TWO;
+}
