efd8695644546edab5cef92841acd0b76cfdff7e
Jamie Briggs <jamie.briggs@justice.gov.uk>
2026-05-07 17:06:29 +0100

    Added Pact testing framework and configuration

---
+++ laa-data-claims-notify-service/src/pactTest/java/uk/gov/justice/laa/dstew/payments/notify/AbstractPactTest.java
@@ -0,0 +1,6 @@
+package uk.gov.justice.laa.dstew.payments.notify;
+
+public abstract class AbstractPactTest {
+  public static final String PACT_CONSUMER = "laa-data-claims-notify-service";
+  public static final String PACT_PROVIDER = "laa-data-claims-api";
+}
